2a4a567c5eb27b0e7190bb7282ba90ff9d889167
James H. Nguyen <james@callfire.com>
2018-08-17 08:50:37 +0000

    add minimal setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 # aliases for bootstrapping docker workspace
 
 alias jn-workspace='/data/gitlab/home/ansible/workspace.sh'
+alias jn-workspace-minimal='/data/gitlab/home/ansible/workspace.sh --tags=workspace.bootstrap.minimal'
 alias jn-workspace-enhance='/data/gitlab/home/ansible/workspace.sh -e "workspace_enhance_enabled=True"'
 alias jn-workspace-developmment='/data/gitlab/home/ansible/workspace.sh -e "workspace_development_enabled=True"'
 
