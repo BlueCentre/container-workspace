e99356ec5d6f2b7ce40e55d3c303b27b9373d3d9
James H. Nguyen <james@callfire.com>
2018-08-17 00:08:02 +0000

    save some typing

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,5 @@
 alias jn-workspace='/data/gitlab/home/ansible/workspace.sh'
 alias jn-workspace-enhance='/data/gitlab/home/ansible/workspace.sh -e "workspace_enhance_enabled=True"'
 alias jn-workspace-developmment='/data/gitlab/home/ansible/workspace.sh -e "workspace_development_enabled=True"'
+
+alias jn-env='cd; . .bashrc'
