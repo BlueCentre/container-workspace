d0efdd61ecb6642e406b71bb7e8ee67702692bba
James H. Nguyen <james@callfire.com>
2018-08-17 16:56:28 +0000

    simply aliases

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 # aliases for bootstrapping docker workspace
 
 alias jn-workspace='/data/gitlab/home/ansible/workspace.sh'
-alias jn-workspace-minimal='/data/gitlab/home/ansible/workspace.sh --tags=workspace.bootstrap.minimal'
-alias jn-workspace-enhance='/data/gitlab/home/ansible/workspace.sh -e "workspace_enhance_enabled=True"'
-alias jn-workspace-developmment='/data/gitlab/home/ansible/workspace.sh -e "workspace_development_enabled=True"'
+alias jn-workspace-minimal='/data/gitlab/home/ansible/workspace.sh minimal'
+alias jn-workspace-enhance='/data/gitlab/home/ansible/workspace.sh enhance'
+alias jn-workspace-developmment='/data/gitlab/home/ansible/workspace.sh development'
 
 alias jn-env='cd; . .bashrc'
